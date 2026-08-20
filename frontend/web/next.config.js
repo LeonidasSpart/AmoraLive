6118e09f4e264eea270cebb331a66429b71c003f
Leonidas <284042396+LeonidasSpart@users.noreply.github.com>
2026-08-20 20:07:21 +0000

    fix: use .jsx extension for pages and add next.config

--- frontend/web/next.config.js
+++ frontend/web/next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['api.amoramatch.one', 'amoramatch.one'],
-  },
+  pageExtensions: ['jsx', 'js', 'tsx', 'ts'],
 };
 module.exports = nextConfig;
